c202fb69ca0cde9954f86df504e28b4579684062
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-09 15:12:47 +0100

    Run app

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname)); 
 
-// routes
+// define home route
 app.get("/", function(req, res){
     res.render("index");
 })
 
-// request and response
+// Use middlewares to set view engine and post json data to the server
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// start the server
 app.listen(port, function(){
-    console.log("app running")
+    console.log("app running on Port: ", port)
 })
