60350b2832638c06430d9ae24b24bd5448537743
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-17 13:00:42 +0100

    db update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
 
 // Database
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://scissoroo_admin:scissoroo_admin@scissoroodb-vjd2z.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb://localhost:27017//scissoroo_admin:scissoroo_admin@scissoroodb-vjd2z.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
