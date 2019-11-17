8f91d16a98145867a830c46408b27364250c9cae
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-17 12:40:22 +0100

    connect mongodb

--- app.js
+++ app.js
@@ -11,12 +11,24 @@ app.set('views',(__dirname));
 app.set('view engine','ejs');
 app.get('./', (req, res) => res.render('index'));
 
-/// catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
+
+// Database
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://scissoroo_admin:scissoroo_admin@scissoroodb-vjd2z.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("database connection on")
+  client.close();
+});
+
 
 // start the server
 app.listen(port, function(){
