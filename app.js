8437d4ca65d10e4975670d6aab3cb6c22f5e2a21
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-20 13:35:03 +0100

    change db

--- app.js
+++ app.js
@@ -41,26 +41,6 @@ app.get('/thelist', function(req, res){
   });
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-
-// Database
-//const MongoClient = require('mongodb').MongoClient;
-//const uri = "mongodb://localhost:27017//scissoroo_admin:scissoroo_admin@scissoroodb-vjd2z.mongodb.net/test?retryWrites=true&w=majority";
-//const client = new MongoClient(uri, { useNewUrlParser: true });
-//client.connect(err => {
-//  const collection = client.db("test").collection("devices");
-//  // perform actions on the collection object
-//  console.log("database connection on")
-//client.close();
-//});
-
-
 // start the server
 app.listen(port, function(){
     console.log("app running on Port: ", port)
