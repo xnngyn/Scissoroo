7c7705c6616af789c423b8946828a9d52beb9e82
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-20 15:37:10 +0100

    database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var assert = require('assert');
 
 var url = 'mongodb://localhost:27017//scissoroo_admin:scissoroo_admin@scissoroodb-vjd2z.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -9,4 +10,19 @@ router.get('/', function(req, res, next){
     res.render('index');
 });
 
+router.get('/get-data', function(req, res, next){
+
+});
+
+router.post('/insert', function(req, res, next){
+
+});
+
+router.post('/update', function(req, res, next){
+
+});
+
+router.post('/delete', function(req, res, next){
+
+});
 module.exports = router;
