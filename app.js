00c590525d1930436f05367c384f2e0af2433430
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-21 16:44:56 +0100

    rechange

--- app.js
+++ app.js
@@ -8,21 +8,12 @@ var routes = require('./routes/index');
 const port = process.env.PORT || 8080;
 
 //view engine setup
-app.set('views', (__dirname + './views'));
-app.set('view engine', 'ejs');
-
+app.use(express.static(__dirname + 'views')); 
+app.set('views',(__dirname));
+app.set('view engine','ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.static(__dirname + './public'));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
-
-//app.use(express.static(__dirname + '/public')); 
-//app.set('views',(__dirname));
-//app.set('view engine','ejs');
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
