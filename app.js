fa255e26dec4f15dae0cc89115806f766b7d3e81
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-21 16:13:31 +0100

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname + '/views'));
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.use('/', routes);
 
