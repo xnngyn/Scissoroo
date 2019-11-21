1d675d4735ac81c5ed079c21d8bc44172299a742
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-21 16:58:51 +0100

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8080;
 
 //view engine setup
 app.use(express.static(__dirname + '/views')); 
-app.set('views',(__dirname, 'views'));
+app.set('views',(__dirname, '/public'));
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
