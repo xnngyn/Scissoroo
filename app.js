9259c010a9e7e4e5e35a8dfec812cc3d64c84c00
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-21 16:31:19 +0100

    test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname, 'public'));
 
 app.use('/', routes);
 
