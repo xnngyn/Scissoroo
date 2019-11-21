8d67c97b0fa4d902deea26f548fafa71e53a7a00
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-21 16:16:45 +0100

    app

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var routes = require('./routes/index');
 const port = process.env.PORT || 8080;
 
 //view engine 
-app.set('views', (__dirname + '/views'));
+//app.set('views', (__dirname + '/views'));
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.use('/', routes);
 
