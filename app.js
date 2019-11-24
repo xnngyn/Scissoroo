b9baf7854b59feba2be4d17845b32704843eecd4
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-24 10:04:45 +0100

    test

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var path = require('path')
 var app = express();
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 
 // set port
 const port = process.env.PORT || 8080;
 
 //view engine setup
-app.use(express.static('./public')); 
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public'))); 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
