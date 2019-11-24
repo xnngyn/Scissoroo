cdf86aeed8f2ab76f4af4094f2f9e7495f263a5c
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-24 10:12:57 +0100

    test

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ const port = process.env.PORT || 8080;
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); 
+
+
 app.use('/', routes);
 
 // catch 404 and forward to error handler
