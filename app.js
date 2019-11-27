864fcdb661ed2ac493e33396018a112e125ebf19
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-27 17:43:18 +0100

    ejs render

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 8080;
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine','html');
+//app.engine('html', require('ejs').renderFile);
+app.set('view engine','ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
