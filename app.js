03ab74901f87ae65b63a4ee81ce4f2fa0f8f39e3
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-11-30 13:50:01 +0100

    change routing

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes);         // all references in routes starts with /
+app.use('/users', users);     // all references in users starts with /users
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
