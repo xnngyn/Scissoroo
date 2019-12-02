1432567e24655c059a742f71662173fa81b9cfc4
Xuan-Anh Nguyen <xuan.anh.nguyen@hotmail.de>
2019-12-02 15:47:45 +0100

    add User var

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var assert = require('assert');
 const bcrypt = require('bcrypt');
+var User = require('../models/user');
 
 var url = 'mongodb+srv://scissoroo_admin:scissoroo_admin@scissoroodb-vjd2z.mongodb.net/scissoroo?retryWrites=true&w=majority';
 
